3e52d153802e5a5d50f42572aa8385bb776a6e72
Jake Bentvelzen <doogie1012@gmail.com>
2018-08-02 16:49:45 +1000

    Fix ss4 invalid props (#21)
    * fix(ComponentTest): Prevent invalid / reserved props being passed in and add test - Github Issue #15
    
    * fix(ComponentData): Remove unused properties and improve documentation

--- src/SilbinaryWolf/Components/ComponentData.php
+++ src/SilbinaryWolf/Components/ComponentData.php
@@ -15,21 +15,31 @@ class ComponentData extends ViewableData
      */
     protected $____name;
 
-    /**
-     * NOTE(Jake): 2018-04-06
-     *
-     * We use a custom class instead of ArrayData to avoid
-     * property clashing.
-     *
-     * (ie. passing an property 'class' to ArrayData won't work as
-     * it'll just utilize the class name "ArrayData")
-     */
     public function __construct($name, array $props)
     {
         parent::__construct();
+
+        // NOTE(Jake): 2018-08-02
+        //
+        // Don't allow use of invalid properties.
+        // ie. You can't pass "failover" as it's used by ViewableData
+        //
+        // The invalid ViewableData properties are at time of writing:
+        // - $failover
+        // - $customisedObject
+        // - $extension_instances
+        // - $beforeExtendCallbacks
+        //
+        foreach (get_object_vars($this) as $prop => $value) {
+            if (isset($props[$prop])) {
+                throw new ComponentReservedPropertyException($name, $prop);
+            }
+        }
+
+        //
         $this->____name = $name;
-        foreach ($props as $name => $value) {
-            $this->{$name} = $value;
+        foreach ($props as $prop => $value) {
+            $this->{$prop} = $value;
         }
     }
 
