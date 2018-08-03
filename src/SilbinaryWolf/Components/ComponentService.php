d69aa4b56d84ab7758f9df680b290614aa07b6a1
Jake Bentvelzen <doogie1012@gmail.com>
2018-08-03 13:16:05 +1000

    feat(ComponentService): Add special _json property for giving a JSON structure to a component (#24)
    * feat(ComponentService): Add special _json property for giving a JSON structure to a component
    
    * feat(ComponentService): Add nicer error handling, add test for error handling and add documentation
    
    * fix(ComponentService): Fix missing Exception use
    
    * fix(ComponentService): Fix phpcs formatting issues
    
    * fix(composer.json): Fix branch alias

--- src/SilbinaryWolf/Components/ComponentService.php
+++ src/SilbinaryWolf/Components/ComponentService.php
@@ -2,6 +2,7 @@
 
 namespace SilbinaryWolf\Components;
 
+use Exception;
 use SilverStripe\View\SSViewer;
 use SilverStripe\View\SSViewer_Scope;
 use SilverStripe\ORM\FieldType\DBHTMLText;
@@ -10,12 +11,15 @@ use SilverStripe\View\SSTemplateParser;
 use SilverStripe\View\SSTemplateParseException;
 use SilverStripe\View\ArrayData;
 use SilverStripe\ORM\SS_List;
+use SilverStripe\ORM\ArrayList;
 use SilverStripe\ORM\DataObject;
 use SilverStripe\ORM\FieldType\DBString;
 use SilverStripe\Core\Config\Config;
 
 class ComponentService
 {
+    const DEBUG_JSON_CODE_OUTPUT = false;
+
     private static $component_paths = [
         'components'
     ];
@@ -28,6 +32,7 @@ class ComponentService
     public function generateTemplateCode(array $res, $parser)
     {
         $thisClassIdent = self::class.'::class';
+        $debugHasUsedJSON = false;
 
         $componentName = $res['ComponentName']['text'];
         $arguments = array();
@@ -54,42 +59,83 @@ class ComponentService
                 throw new SSTemplateParseException('Cannot use < % loop % > inside property "'.$propertyName.'" on component "'.$componentName.'"', $parser);
             }
 
-            // Modify provided PHP code
-            $ifValueParts = explode('[_CPB]', $valueParts);
+            //
             $phpCodeValueParts = array();
-            foreach ($ifValueParts as $value) {
-                $value = trim($value);
-                // NOTE(Jake): 2018-04-29
-                //
-                // Remove blank string concats from template. This is to avoid an object from
-                // being cast to a string, like getAttributesHTML().
-                //
-                // We want to avoid this so that in SS4, there attributes aren't double quoted as
-                // HTML is escaped by default.
-                //
-                $valueParts = explode('[_CFP]', $value);
-                foreach ($valueParts as $valuePart) {
-                    if ($valuePart === "''") {
-                        // Skip empty string parts
-                        continue;
-                    }
-                    $valuePart = trim($valuePart);
-                    if (strpos($valuePart, '$val .=') !== false) {
-                        if (strpos($valuePart, 'XML_val(') !== false) {
-                            // NOTE(Jake): 2018-04-29, This hack is for inside an <% if %>
-                            $valuePart = str_replace(array('XML_val(', ';'), array('obj(', '->self();'), $valuePart);
+            if ($propertyName &&
+                $propertyName[0] === '_') {
+                // Handle special variable (ie. prefixed with _)
+                $propertyNameWithPrefix = $propertyName;
+                $propertyName = substr($propertyNameWithPrefix, 1);
+                switch ($propertyName) {
+                    case 'json':
+                        $jsonString = $valueParts;
+                        $jsonString = trim($jsonString);
+                        $jsonString = trim($jsonString, '\'');
+                        $jsonString = trim($jsonString, '"');
+                        $jsonData = @json_decode($jsonString, true);
+                        if (json_last_error() !== JSON_ERROR_NONE) {
+                            // todo(Jake): 2018-08-03
+                            //
+                            // See if we can use an alterate parser that can give better error messages.
+                            //
+                            switch (json_last_error()) {
+                                case JSON_ERROR_SYNTAX:
+                                    throw new SSTemplateParseException('JSON Syntax error, did you quote all the property names and remove trailing commas? I suggest running the following through a JSON validator online.'."\n".$jsonString, $parser);
+                                    break;
+                            }
+                            throw new SSTemplateParseException('JSON '.json_last_error_msg()."\n".$jsonString, $parser);
                         }
-                        $valuePart = str_replace('$val .=', '$_props[\''.$propertyName.'\'][] =', $valuePart);
+                        foreach ($jsonData as $propertyName => $value) {
+                            if (is_array($value)) {
+                                $value = 'new '.ArrayList::class.'('.var_export($value, true).')';
+                            }
+                            $phpCodeValueParts[] = "\$_props['".$propertyName."'][] = ".$value.";";
+                        }
+                        $debugHasUsedJSON = true;
+                        break;
+
+                    default:
+                        throw new SSTemplateParseException('Invalid special property type: "'.$propertyNameWithPrefix.'", properties that start with a _ are reserved for special functionality. Available special property types are: "_json".', $parser);
+                        break;
+                }
+            } else {
+                // Modify provided PHP code
+                $ifValueParts = explode('[_CPB]', $valueParts);
+                foreach ($ifValueParts as $value) {
+                    $value = trim($value);
+                    // NOTE(Jake): 2018-04-29
+                    //
+                    // Remove blank string concats from template. This is to avoid an object from
+                    // being cast to a string, like getAttributesHTML().
+                    //
+                    // We want to avoid this so that in SS4, there attributes aren't double quoted as
+                    // HTML is escaped by default.
+                    //
+                    $valueParts = explode('[_CFP]', $value);
+                    foreach ($valueParts as $valuePart) {
+                        if ($valuePart === "''") {
+                            // Skip empty string parts
+                            continue;
+                        }
+                        $valuePart = trim($valuePart);
+                        if (strpos($valuePart, '$val .=') !== false) {
+                            if (strpos($valuePart, 'XML_val(') !== false) {
+                                // NOTE(Jake): 2018-04-29, This hack is for inside an <% if %>
+                                $valuePart = str_replace(array('XML_val(', ';'), array('obj(', '->self();'), $valuePart);
+                            }
+                            $valuePart = str_replace('$val .=', '$_props[\''.$propertyName.'\'][] =', $valuePart);
+                            $phpCodeValueParts[] = $valuePart;
+                            //$phpCode .= $valuePart."\n";
+                            continue;
+                        }
+                        $valuePart = "\$_props['".$propertyName."'][] = ".$valuePart.";";
                         $phpCodeValueParts[] = $valuePart;
                         //$phpCode .= $valuePart."\n";
-                        continue;
                     }
-                    $valuePart = "\$_props['".$propertyName."'][] = ".$valuePart.";";
-                    $phpCodeValueParts[] = $valuePart;
-                    //$phpCode .= $valuePart."\n";
                 }
             }
 
+            //
             $phpCode = "";
             if (count($phpCodeValueParts) === 1) {
                 //$phpCode .= "\$_props['".$propertyName."'] = \$_props['".$propertyName."'][0];\n";
@@ -128,6 +174,11 @@ class ComponentService
 \$val .= \SilverStripe\Core\Injector\Injector::inst()->get({$thisClassIdent})->renderComponent('$componentName', \$_props, \$scope);
 unset(\$_props);
 PHP;
+        if (self::DEBUG_JSON_CODE_OUTPUT &&
+            $debugHasUsedJSON) {
+            var_dump($result);
+            throw new Exception('Debug stop.');
+        }
         return $result;
     }
 
--- tests/ComponentTest.php
+++ tests/ComponentTest.php
@@ -13,6 +13,7 @@ use SilverStripe\Forms\TextField;
 use SilverStripe\View\ViewableData;
 use SilverStripe\View\SSViewer_Scope;
 use SilverStripe\Core\Injector\Injector;
+use SilverStripe\View\SSTemplateParseException;
 use SilbinaryWolf\Components\ComponentService;
 use SilbinaryWolf\Components\ComponentReservedPropertyException;
 
@@ -409,6 +410,88 @@ SSTemplate;
             return;
         }
         $this->fail('No exception thrown. Expected '.ComponentReservedPropertyException::class.' to be thrown.');
+    }
+
+    /**
+     * Test custom JSON syntax that allows arbitrary JSON.
+     *
+     */
+    public function testJSONProperty()
+    {
+        $template = <<<SSTemplate
+<:JSONSyntaxTest
+    _json='{
+        "Cards": [
+            {
+                "Title": "This is the first card",
+                "Summary": "This is the first card summary",
+                "Link": "https://link1.com"
+            },
+            {
+                "Title": "This is the second card",
+                "Summary": "This is the second card summary",
+                "Link": "https://link2.com"
+            }
+        ]
+    }'
+/>
+SSTemplate;
+        $expectedHTML = <<<HTML
+        <div>
+            <h2>This is the first card</h2>
+            <p>This is the first card summary</p>
+            <a href="https://link1.com">Read more</a>
+        </div>
+        <div>
+            <h2>This is the second card</h2>
+            <p>This is the second card summary</p>
+            <a href="https://link2.com">Read more</a>
+        </div>
+HTML;
+
+        $resultHTML = SSViewer::fromString($template)->process(null);
+        $this->assertEqualIgnoringWhitespace($expectedHTML, $resultHTML, 'Unexpected output');
+    }
+
+    /**
+     * Test that the error message given when using JSON is useful when debugging.
+     *
+     */
+    public function testJSONPropertyErrorHandling()
+    {
+        $template = <<<SSTemplate
+<:JSONSyntaxTest
+    _json='{
+        "Cards": [
+            {
+                "Title": "This is the first card",
+                "Summary": "This is the first card summary",
+                "Link": "https://link1.com",
+            },
+            {
+                "Title": "This is the second card",
+                "Summary": "This is the second card summary",
+                "Link": "https://link2.com"
+            }
+        ]
+    }'
+/>
+SSTemplate;
+        try {
+            SSViewer::fromString($template)->process(null);
+        } catch (SSTemplateParseException $e) {
+            // Success path!
+            $expectedMessage = 'Parse error in template on line 16. Error was: JSON Syntax error, did you quote all the property names and remove trailing commas? I suggest running the following through a JSON validator online.';
+            $this->assertContains(
+                $expectedMessage,
+                $e->getMessage(),
+                'Unexpected exception message given. Was this changed? If so, please update the expected value above.'
+            );
+            return;
+        } catch (Exception $e) {
+            $this->fail('Incorrect Exception caught. Expected '.SSTemplateParseException::class.' to be thrown.');
+            return;
+        }
     }
 
     /**
